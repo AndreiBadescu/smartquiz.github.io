3d83d8da5de49f37e84cc3e3a5f3e3844c93fe47
AndreiBadescu <34285711+AndreiBadescu@users.noreply.github.com>
2020-04-01 14:19:43 +0300

    Update Q&A.js

--- js/Q&A.js
+++ js/Q&A.js
@@ -1,32 +1,32 @@
 questions = [
 	{
-	question: "Care este capitala tarii Tuvalu ?",
-	answers: [
-	    "Funafuti",
-	    "Futefuti",
-	    "Fugifa",
-	    "Funiefufa"
-	],
-	key: 0
-	},
-	{
-	question: "Care este cel mai puternic drog din cele de mai jos ?",
-	answers: [
-	    "AlaCool",
-	    "Ca-n Abis",
-	    "CoFaina",
-	    "LeeSeTe"
-	],
-	key: 3
-	},
-	{
-	question: "Care este numele virusului care a produs pandemia din 2019 ?",
-	answers: [
-	    "Covid-19",
-	    "Sars-Cov-2",
-	    "Coronavirus",
-	    "A"
-	],
-	key: 1
-	}
+        question: "Care este cel mai puternic drog din cele de mai jos ?",
+        answers: [
+            "AlaCool",
+            "Ca-n Abis",
+            "CoFaina",
+            "LeeSeTe"
+        ],
+        key: 3
+    },
+    {
+        question: "Care este capitala tarii Tuvalu ?",
+        answers: [
+            "Funafuti",
+            "Futefuti",
+            "Fugifa",
+            "Funiefufa"
+        ],
+        key: 0
+    },
+    {
+        question: "Care este numele virusului care a produs pandemia din 2019 ?",
+        answers: [
+            "Covid-19",
+            "Sars-Cov-2",
+            "Coronavirus",
+            "A"
+        ],
+        key: 1
+    }
 ];
